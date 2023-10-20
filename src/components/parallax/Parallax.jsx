b92ff0fb6ca3e1dab58ec4831534bdf705a4a0db
drhcode <doriancocka@gmail.com>
2023-10-20 18:29:13 +0200

    fixed some animation

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -10,26 +10,30 @@ const Parallax = ({ type }) => {
     offset: ['start start', 'end start'],
   });
 
-  const YText = useTransform(scrollYProgress, [0, 1], ['0%', '500%']);
+  const yText = useTransform(scrollYProgress, [0, 1], ['0%', '500%']);
   const yBg = useTransform(scrollYProgress, [0, 1], ['0%', '100%']);
 
   return (
     <div
       className='parallax'
+      ref={ref}
       style={{
         background:
           type === 'services'
-            ? 'linear-gradient(180deg, #111132, #0c0c1d'
-            : 'linear-gradient(180deg, #111132, #505064',
+            ? 'linear-gradient(180deg, #111132, #0c0c1d)'
+            : 'linear-gradient(180deg, #111132, #505064)',
       }}
     >
-      <motion.h1 style={{ y: YText }}>
-        {type === 'services' ? 'What we do?' : 'What we did?'}
+      <motion.h1 style={{ y: yText }}>
+        {type === 'services' ? 'What We Do?' : 'What We Did?'}
       </motion.h1>
       <motion.div className='mountains'></motion.div>
       <motion.div
         className='planets'
-        style={{ y: yBg, background: `url(${type === 'services' ? '/planets.png' : '/sun.png'})` }}
+        style={{
+          y: yBg,
+          backgroundImage: `url(${type === 'services' ? '/planets.png' : '/sun.png'})`,
+        }}
       ></motion.div>
       <motion.div style={{ x: yBg }} className='stars'></motion.div>
     </div>
